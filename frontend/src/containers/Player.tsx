d480ea357c114f51c428fd2527ef8e8b2071e5e9
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-05-27 15:47:31 -0300

    Style PlaylistHeader and ControlButtons

--- frontend/src/containers/Player.tsx
+++ frontend/src/containers/Player.tsx
@@ -1,7 +1,16 @@
+import {headerInfo, tracks} from '../services/api';
+import PlaylistHeader from './components/PlaylistHeader';
+import PlaylistItem from './components/PlaylistItem';
+
 const Player = () => {
     return (
-        <div>
-            <h3>Player</h3>
+        <div className="player-container">
+            <PlaylistHeader {... headerInfo}/>
+            {
+                tracks.map(track => (
+                    <PlaylistItem {... track} key={track.id}/>
+                ))
+            }
         </div>
     );
 };
--- frontend/src/containers/components/PlaylistHeader.tsx
+++ frontend/src/containers/components/PlaylistHeader.tsx
@@ -1,7 +1,21 @@
-const PlaylistHeader = () => {
+import playlistCover from '../../assets/images/cover/playlist.png';
+
+type HeaderProps = {
+    title: string;
+    subtitle: string;
+};
+
+const PlaylistHeader = ({title,subtitle}: HeaderProps) => {
     return (
-        <div>
-            <h3>PlaylistHeader</h3>
+        <div className="header-container">
+            <div className="header-background" />
+            <div className="header-content">
+                <img src={playlistCover} alt="Capa da Playlist" className="header-cover" />
+                <div className="info">
+                    <h3>{title}</h3>
+                    <h6>{subtitle}</h6>
+                </div>
+            </div>
         </div>
     );
 };
